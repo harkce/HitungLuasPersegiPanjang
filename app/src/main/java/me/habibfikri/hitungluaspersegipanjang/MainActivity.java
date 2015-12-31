123c381a026bec0f3280e69f696099ae35a92c2d
harkce <habibgarut@gmail.com>
2015-12-31 17:53:42 +0700

    Main Activity, hitung luasnya

--- app/src/main/java/me/habibfikri/hitungluaspersegipanjang/MainActivity.java
+++ app/src/main/java/me/habibfikri/hitungluaspersegipanjang/MainActivity.java
@@ -2,6 +2,10 @@ package me.habibfikri.hitungluaspersegipanjang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText txtPanjang = (EditText) findViewById(R.id.edt_panjang);
+        final EditText txtLebar = (EditText) findViewById(R.id.edt_lebar);
+        Button btnHitung = (Button) findViewById(R.id.btn_hitung);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double panjang, lebar;
+                if(txtPanjang.getText().toString().equals(""))
+                    panjang = 0;
+                else
+                    panjang = Double.parseDouble(txtPanjang.getText().toString());
+                if(txtLebar.getText().toString().equals(""))
+                    lebar = 0;
+                else
+                    lebar = Double.parseDouble(txtLebar.getText().toString());
+                TextView txtHasil = (TextView) findViewById(R.id.txt_luas);
+                double luas = panjang * lebar;
+                txtHasil.setText("Luas : " + luas);
+            }
+        });
     }
 }
